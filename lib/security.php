9817435456adc2bcb0ea2f326a20ecfa6815d5af
Robert Erickson <robert.erickson@uvm.edu>
2014-06-23 16:45:25 -0400

    Extra items for adding other elements, some html 5 css and a minor mistake I forgot to declare a variable

--- lib/security.php
+++ lib/security.php
@@ -5,6 +5,7 @@
 function securityCheck($form = false) {
     
     $status = true; // start off thinking everything is good until a test fails
+    $debug = false; 
     
     // when it is a form page check to make sure it submitted to itself
     if ($form) {
