9c369e019b2dcdb304477d25027c2c7a0f9c50d3
Robert Erickson <robert.erickson@uvm.edu>
2014-06-23 15:21:48 -0400

    Create the html form to accept a users email address and submit the form. Remember the blank space and comments are there so you can see the whole process unfold without line numbers changing. Add css to display the form centered and to align the label and text box.

--- form.php
+++ form.php
@@ -1,9 +1,320 @@
 <?php
 include "top.php";
-?>
-<article id="main">
-    <h1>Form</h1>
+
+//%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+//
+// SECTION: 1 Initialize variables
+//
+// SECTION: 1a.
+// variables for the classroom purposes to help find errors.
+
+
+
+
     
+
+
+
+    
+
+//%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+//
+// SECTION: 1b Security
+//
+// 
+
+
+
+//%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+//
+// SECTION: 1c form variables
+//
+// 
+// 
+
+
+
+
+//%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+//
+// SECTION: 1d form error flags
+//
+//
+//
+
+
+
+//%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+//
+// SECTION: 1e misc variables
+//
+// 
+
+
+
+
+
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//
+// SECTION: 2 Process for when the form is submitted
+//
+
+
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //
+    // SECTION: 2a Security
+    // 
+    
+        
+        
+        
+    
+    
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //
+    // SECTION: 2b Sanitize (clean) data 
+    // 
+    // 
+
+    
+    
+
+
+    
+    
+
+
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //
+    // SECTION: 2c Validation
+    //
+    // 
+    // 
+    // 
+    //  
+    // 
+    // 
+
+    
+        
+        
+    
+        
+        
+    
+
+    
+        
+        
+    
+        
+        
+    
+
+
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //
+    // SECTION: 2d Process Form - Passed Validation
+    //
+    // 
+    //
+    
+        
+            
+
+        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        //
+        // SECTION: 2e Save Data
+        //
+        // 
+
+        
+
+        
+
+        
+
+        
+            
+
+        
+        
+
+        
+        
+
+        
+        
+
+        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        //
+        // SECTION: 2f Create message
+        //
+        // 
+        // 
+
+        
+
+        
+
+            
+
+            
+
+            
+                
+            
+            
+        
+
+
+        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        //
+        // SECTION: 2g Mail to user
+        //
+        // 
+        // 
+        
+        
+        
+        
+
+        
+        
+        
+
+        
+        
+    
+    
+
+
+//#############################################################################
+//
+// SECTION 3 Display Form
+//
+?>
+
+<article id="main">
+
+    <?php
+    //####################################
+    //
+    // SECTION 3a.
+    //
+    // 
+    // 
+    // 
+    // 
+    // 
+    
+        
+
+        
+            
+        
+
+        
+
+        
+        
+            
+        
+        
+        
+        
+
+        
+    
+
+
+        //####################################
+        //
+        // SECTION 3b Error Messages
+        //
+        // 
+
+        
+            
+            
+            
+                
+            
+            
+            
+        
+
+
+        //####################################
+        //
+        // SECTION 3c html Form
+        //
+        /* Display the HTML form. note that the action is to this same page. $phpSelf
+          is defined in top.php
+          
+
+          
+
+          
+          
+
+          
+
+          
+
+          
+          
+
+         */
+        ?>
+
+        <form action="<?php print $phpSelf; ?>"
+              method="post"
+              id="frmRegister">
+
+            <fieldset class="wrapper">
+                <legend>Register Today</legend>
+                <p>You information will greatly help us with our research.</p>
+
+                <fieldset class="wrapperTwo">
+                    <legend>Please complete the following form</legend>
+
+                    <fieldset class="contact">
+                        <legend>Contact Information</legend>
+                        
+                            
+                                   
+                                   
+                                   
+                                   
+                                   
+                        
+                        
+                        <label for="txtEmail" class="required">Email
+                            <input type="text" id="txtEmail" name="txtEmail"
+                                   value=""
+                                   tabindex="120" maxlength="45" placeholder="Enter a valid email address"
+                                   
+                                   onfocus="this.select()" 
+                                   autofocus>
+                        </label>
+                    </fieldset> <!-- ends contact -->
+                    
+                </fieldset> <!-- ends wrapper Two -->
+                
+                <fieldset class="buttons">
+                    <legend></legend>
+                    <input type="submit" id="btnSubmit" name="btnSubmit" value="Register" tabindex="900" class="button">
+                </fieldset> <!-- ends buttons -->
+                
+            </fieldset> <!-- Ends Wrapper -->
+        </form>
+
+    <?php
+     
+    ?>
+
 </article>
 
 <?php include "footer.php"; ?>
